8f881c58b68b062e78303bcc80fcf14f30c585b6
KolBbl4 <ko1ba@mail.ru>
2024-07-19 14:49:23 +0500

    Тестовый пример

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
-import os
+import requests
+from bs4 import BeautifulSoup
 
 def main():
-    pass
+    url = 'ya.ru'
+    #headers = {};
+    reg = requests.get(url)
+    print( reg.content)
 
 def con():
     pass
+
+if __name__ == "__main__":
+    main()
